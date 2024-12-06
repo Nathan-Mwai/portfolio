1fd40dcead0dc18299793ef8285d7cf3dd180b06
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-07 00:07:45 +0300

    Made a different place for where data will be extracted

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+export const gridItems = [
+    { id:1, title: "Title1", description:'Desc1'},
+    { id:2, title: "Title1", description:'Desc1'},
+    { id:3, title: "Title1", description:'Desc1'},
+    { id:4, title: "Title1", description:'Desc1'},
+    { id:5, title: "Title1", description:'Desc1'},
+    { id:6, title: "Title1", description:'Desc1'},
+  ]
