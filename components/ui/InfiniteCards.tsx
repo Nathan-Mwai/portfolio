e0e7e5695e816994ba679c83a378ed41972f0ddb
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-08 13:20:52 +0300

    Added collaboration under my name

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -73,7 +73,6 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        // max-w-7xl to w-screen
         "scroller relative z-20 w-screen overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
