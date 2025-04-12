5780f992e0b01612bc68e91d0fdafce2afa53b1c
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-12 19:48:23 +0300

    Implement NavBar for project navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
-  return (
-    <>
-        <Hero/>
-        <ShowcaseSection/>
-    </>
-  )
+    return (
+        <>
+            <NavBar/>
+            <Hero/>
+            <ShowcaseSection/>
+        </>
+    )
 }
 
 export default App
