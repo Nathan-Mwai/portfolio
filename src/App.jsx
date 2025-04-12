713f6110b0dc6a362534f2b130e777e6826ea78c
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-12 08:33:44 +0300

    implement showcase of recent projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 
 const App = () => {
   return (
     <>
         <Hero/>
+        <ShowcaseSection/>
     </>
   )
 }
