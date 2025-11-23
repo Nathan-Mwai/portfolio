5541091d6f2ce4af06cd2a607a9454c68d53bea8
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-11-23 06:35:56 +0300

    Implement initial tsx section of the dock

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "@/components/Navbar";
 import Welcome from "@/components/Welcome";
+import Dock from "@/components/Dock";
 
 const Page = () => {
     return (
         <main>
             <Navbar/>
             <Welcome/>
+            <Dock/>
         </main>
     )
 }
